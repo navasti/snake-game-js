72bd43e7cd394c6fbb2426e44c6d3b44d5ea6664
Paweł Szostak <szostak_pawel@outlook.com>
2020-06-12 15:59:27 +0200

    Finished the game

--- game.js
+++ game.js
@@ -1,9 +1,25 @@
-import { SNAKE_SPEED, update as updateSnake, draw as drawSnake } from './snake.js';
+import {
+  SNAKE_SPEED,
+  update as updateSnake,
+  draw as drawSnake,
+  getSnakeHead,
+  snakeIntersection
+} from './snake.js';
+import { outsideGrid } from './grid.js';
+import { update as updateFood, draw as drawFood } from './food.js';
 
 let lastRenderTime = 0;
+let gameOver = false;
 const gameBoard = document.querySelector('#game-board');
 
 function main(currentTime) {
+  if (gameOver) {
+    if (confirm('You lost. Press OK to restart.')) {
+      window.location = '/';
+    };
+    return;
+  };
+
   window.requestAnimationFrame(main);
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
   if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
@@ -18,8 +34,16 @@ window.requestAnimationFrame(main);
 
 function update() {
   updateSnake();
+  updateFood();
+  checkFailure();
 };
+
 function draw() {
   gameBoard.innerHTML = '';
   drawSnake(gameBoard);
+  drawFood(gameBoard);
+};
+
+function checkFailure() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
 };
